3ad2bb60e3638e69e618a80a4eebdd5f0df7db2c
X1atle <myron.ryzleitsev@gmail.com>
2023-11-27 07:02:03 +0100

    js file added

---
+++ main.js
@@ -0,0 +1,4 @@
+function toggleOverlay() {
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = (overlay.style.display === "none" || overlay.style.display === "") ? "flex" : "none";
+}
